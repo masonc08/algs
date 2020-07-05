21cf581c484d34b749c3c86c53b4e3a98ea480d6
Mason <mxmcao@gmail.com>
2020-07-04 22:33:43 -0400

    complete quicksort implementation

--- quick_sort.py
+++ quick_sort.py
@@ -10,16 +10,13 @@ def quick_sort(nums):
     pivot = nums[-1]
     i, j = 0, len(nums) - 2
     while 1:
-        if nums[i] > pivot >= nums[j]:
-            swap(nums, i, j)
-        if i >= j:
-            if nums[i] >= pivot:
-                swap(nums, i, -1)
-            return quick_sort(nums[:i]) + [nums[i]] + quick_sort(nums[i+1:])
-        if nums[i] < pivot and i != j:
+        while nums[i] < pivot and i != j:
             i += 1
-        if nums[j] >= pivot and i != j:
+        while nums[j] >= pivot and i != j:
             j -= 1
-
-
-print(quick_sort([2, 1, 8, 9, 3]))
+        if i != j:
+            swap(nums, i, j)
+        else:
+            swap(nums, i, -1)
+            nums = quick_sort(nums[:i]) + [nums[i]] + quick_sort(nums[i+1:])
+            return nums
