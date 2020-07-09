76ec01fdf6160f24d2b6a967f74a01dd9cd3e5c1
Mason <mxmcao@gmail.com>
2020-07-09 09:45:18 -0400

    impprove leetcode202 using floyd cycle detection

--- happy_number.py
+++ happy_number.py
@@ -3,15 +3,23 @@
 
 class Solution:
     def isHappy(self, n):
-        seen = set()
-        while n != 1 and n not in seen:
-            sum_of_sqs = 0
-            seen.add(n)
-            while n != 0:
-                sum_of_sqs += (n % 10) * (n % 10)
-                n //= 10
-            n = sum_of_sqs
-        return n == 1
+        x = n
+        y = n
+        while 1:
+            x = Solution._next_num(x)
+            if x == 1:
+                return True
+            y = Solution._next_num(Solution._next_num(y))
+            if y == 1:
+                return True
+            if x == y:
+                return False
 
+    @staticmethod
+    def _next_num(n):
+        sum_of_sqs = 0
+        while n != 0:
+            sum_of_sqs += (n % 10) * (n % 10)
+            n //= 10
+        return sum_of_sqs
 
-print(Solution().isHappy(19))
