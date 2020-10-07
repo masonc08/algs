37ce15f4dede342509259580374587f27f45e0e9
Mason <mxmcao@gmail.com>
2020-10-07 10:08:20 -0400

    change comparator for largest element on top

--- sort_stack.py
+++ sort_stack.py
@@ -4,14 +4,9 @@ def sort_stack(s1):
     s2 = []
     while s1:
         temp = s1.pop()
-        xfered = 0
-        while len(s2) != 0 and s2[-1] < temp:
+        while len(s2) != 0 and s2[-1] > temp:
             s1.append(s2.pop())
-            xfered += 1
         s2.append(temp)
-        while xfered != 0:
-            s2.append(s1.pop())
-            xfered -= 1
     while s2:
         s1.append(s2.pop())
 
