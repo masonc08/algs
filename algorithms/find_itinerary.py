21687febe5cd1b7244c7e412c9f5b33983b68f70
Mason <mxmcao@gmail.com>
2020-10-16 19:21:20 -0400

    add leetcode 207

---
+++ algorithms/find_itinerary.py
@@ -0,0 +1,19 @@
+# Leetcode 207
+
+import collections
+
+
+class Solution:
+    def findItinerary(self, tickets):
+        targets = collections.defaultdict(list)
+        for a, b in sorted(tickets)[::-1]:
+            targets[a] += b,
+        route, stack = [], ['JFK']
+        while stack:
+            while targets[stack[-1]]:
+                stack += targets[stack[-1]].pop(),
+            route += stack.pop(),
+        return route[::-1]
+
+
+Solution().findItinerary([["JFK","KUL"],["JFK","NRT"],["NRT","JFK"]])
