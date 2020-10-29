85de23fd9b8eabe97150804abb1e9f8961024c86
Mason <mason.cao@uwaterloo.ca>
2020-10-29 12:34:09 -0400

    add leetcode 168

---
+++ algorithms/excel_sheet_column_title.py
@@ -0,0 +1,15 @@
+"""
+Leetcode 168
+Runtime: 24 ms, faster than 90.78% of Python3 online submissions for Excel Sheet Column Title.
+Memory Usage: 14.1 MB, less than 100.00% of Python3 online submissions for Excel Sheet Column Title.
+"""
+
+
+class Solution:
+    def convertToTitle(self, n: int) -> str:
+        sol = []
+        while n >= 1:
+            n -= 1
+            sol.append(chr(ord('A') + n%26))
+            n //= 26
+        return ''.join(reversed(sol))
