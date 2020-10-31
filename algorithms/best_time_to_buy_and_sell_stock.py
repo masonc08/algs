5662911df7c4e58a9d30832e3f5d0cd9efe39c38
Mason <mason.cao@uwaterloo.ca>
2020-10-31 13:25:10 -0400

    added dp approach

--- algorithms/best_time_to_buy_and_sell_stock.py
+++ algorithms/best_time_to_buy_and_sell_stock.py
@@ -1,16 +1,34 @@
 """
 Leetcode 121
+"""
+ 
+"""
+Dynamic program using Kadane's Algorithm
+Runtime: 68 ms, faster than 38.75% of Python3 online submissions for Best Time to Buy and Sell Stock.
+Memory Usage: 14.9 MB, less than 99.99% of Python3 online submissions for Best Time to Buy and Sell Stock.
+"""
+class Solution:
+    def maxProfit(self, prices: List[int]) -> int:
+        prev = cur = 0
+        sol = 0
+        for i in range(1, len(prices)):
+            cur = max(0, prev+prices[i]-prices[i-1])
+            sol = max(cur, sol)
+            prev = cur
+        return sol
+
+
+"""
 Runtime: 56 ms, faster than 92.83% of Python3 online submissions for Best Time to Buy and Sell Stock.
 Memory Usage: 15 MB, less than 99.99% of Python3 online submissions for Best Time to Buy and Sell Stock.
 """
-
-class Solution:
-    def maxProfit(self, prices: List[int]) -> int:
-        lowest = (1<<31)-1
-        highest = 0
-        for price in prices:
-            if price < lowest:
-                lowest = price
-            elif price-lowest > highest:
-                highest = price-lowest
-        return highest
+# class Solution:
+#     def maxProfit(self, prices: List[int]) -> int:
+#         lowest = (1<<31)-1
+#         highest = 0
+#         for price in prices:
+#             if price < lowest:
+#                 lowest = price
+#             elif price-lowest > highest:
+#                 highest = price-lowest
+#         return highest
