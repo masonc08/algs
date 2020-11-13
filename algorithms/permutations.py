ce0b77556b2670e0314cee1adc3392eec0891861
Mason <mason.cao@uwaterloo.ca>
2020-11-12 19:06:25 -0500

    update runtime for permutations

--- algorithms/permutations.py
+++ algorithms/permutations.py
@@ -1,3 +1,10 @@
+"""
+Leetcode 46
+Runtime: 44 ms, faster than 34.05% of Python3 online submissions for Permutations.
+Memory Usage: 14.2 MB, less than 91.91% of Python3 online submissions for Permutations.
+"""
+
+
 class Solution:
     @staticmethod
     def permute(nums):
