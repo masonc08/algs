8f7cc06c8a86b4f7f5b0093472e75258bbd436c9
Mason <mason.cao@uwaterloo.ca>
2020-11-16 15:27:24 -0500

    added abstraction for improved solution

--- algorithms/minimum_operations_to_reduce_x_to_zero.py
+++ algorithms/minimum_operations_to_reduce_x_to_zero.py
@@ -5,27 +5,39 @@ Leetcode weekly contest 215
 
 
 import math
+import collections
 from typing import List
 
 
 class Solution:
     """
-    Runtime: 1144 ms, faster than 50.00% of Python3 online submissions for Minimum Operations to Reduce X to Zero.
-    Memory Usage: 35.8 MB, less than 50.00% of Python3 online submissions for Minimum Operations to Reduce X to Zero.
+    Problem abstracted into longest subarray, modified to adapt for negative array input
+    Runtime: 1168 ms, faster than 88.34% of Python3 online submissions for Minimum Operations to Reduce X to Zero.
+    Memory Usage: 36.2 MB, less than 35.71% of Python3 online submissions for Minimum Operations to Reduce X to Zero.
     """
     def minOperations(self, nums: List[int], x: int) -> int:
         target = sum(nums)-x
-        if target == 0:
-            return len(nums)
-        sol = -math.inf
-        cur = 0
-        mp = { 0: -1 }
+        sol = self.longest_subarray(nums, target)
+        return -1 if sol == -1 else len(nums)-sol
+
+    def longest_subarray(self, nums, target):
+        sol, cur = -math.inf, 0
+        mp = { 0: -1 }  # Sum of 0 always exists before start of array at index -1
+        if target == 0:  # 0 is possible solution if target is 0
+            sol = 0
         for i, num in enumerate(nums):
             cur += num
             if cur-target in mp:
                 sol = max(sol, i-mp[cur-target])
-            mp[cur] = i
-        return -1 if sol == -math.inf else len(nums)-sol
+            mp[cur] = max(mp[cur], i) if cur in mp else i
+        return -1 if sol == -math.inf else sol
+
+    def assertions(self):
+        assert self.minOperations([1, 1, 4, 2, 3], 5) == 2
+        assert self.minOperations([1, 1, 2, 3, 4, 5], 0) == 0
+        assert self.minOperations([-3, 1, -4, -3, 6, 4], 1) == 2  # Array sums to -1, but can get it without summing entire array
+        assert self.minOperations([], 0) == 0
+        assert self.minOperations([], -6) == -1
 
     """
     DFS attempt
@@ -40,7 +52,7 @@ class Solution:
     #     def _minOperations(i, j, count, cur):
     #         if cur == x:
     #             return count
-    #         if i == j or cur > x:
+    #         if i == j or cur > x: 
     #             return -1
     #         if cur+nums[i] > x and cur+nums[j] > x:
     #             return -1
@@ -79,5 +91,4 @@ class Solution:
     #         return -1
     #     return _minOperations(0, len(nums)-1, 0, 0)
 
-print(Solution().minOperations([5207,5594,477,6938,8010,7606,2356,6349,3970,751,5997,6114,9903,3859,6900,7722,2378,1996,8902,228,4461,90,7321,7893,4879,9987,1146,8177,1073,7254,5088,402,4266,6443,3084,1403,5357,2565,3470,3639,9468,8932,3119,5839,8008,2712,2735,825,4236,3703,2711,530,9630,1521,2174,5027,4833,3483,445,8300,3194,8784,279,3097,1491,9864,4992,6164,2043,5364,9192,9649,9944,7230,7224,585,3722,5628,4833,8379,3967,5649,2554,5828,4331,3547,7847,5433,3394,4968,9983,3540,9224,6216,9665,8070,31,3555,4198,2626,9553,9724,4503,1951,9980,3975,6025,8928,2952,911,3674,6620,3745,6548,4985,5206,5777,1908,6029,2322,2626,2188,5639], 
-565610))
+Solution().assertions()
