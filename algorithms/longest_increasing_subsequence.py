191410db5cd968b513dff08cb64db982259edf4d
Mason <mason.cao@uwaterloo.ca>
2020-11-18 15:49:39 -0500

    change binary search decisions

--- algorithms/longest_increasing_subsequence.py
+++ algorithms/longest_increasing_subsequence.py
@@ -18,12 +18,16 @@ class Solution:
         dp_size = 0
         for num in nums:
             i, j = 0, dp_size
-            while i != j:
-                mid = (i+j)//2
-                if dp[mid] < num:
-                    i = mid+1
+            while i < j:
+                m = (i+j)//2
+                v = dp[m]
+                if v == num:
+                    i = m
+                    break
+                elif v < num:
+                    i = m+1
                 else:
-                    j = mid
+                    j = m
             dp[i] = num
             if i == dp_size:
                 dp_size += 1
