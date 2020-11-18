137914c4ac3616e4c3b88354d386c16cb2cd701f
Mason <mason.cao@uwaterloo.ca>
2020-11-18 11:08:22 -0500

    add new sol for leetcode 53

--- algorithms/merge_intervals.py
+++ algorithms/merge_intervals.py
@@ -23,3 +23,20 @@ class Solution:
             li.append(new_interval)
         else:
             li.append([a, max(b, d)])
+    
+    """
+    November Leetcoding challenge attempt
+    Runtime: 80 ms, faster than 87.94% of Python3 online submissions for Merge Intervals.
+    Memory Usage: 15.8 MB, less than 66.66% of Python3 online submissions for Merge Intervals.
+    """
+    # def merge(self, intervals: List[List[int]]) -> List[List[int]]:
+    #     intervals.sort(key=lambda x: x[0])
+    #     sol = [intervals[0]]
+    #     for i in range(1, len(intervals)):
+    #         c, d = intervals[i]
+    #         a, b = sol[-1]
+    #         if c <= b:
+    #             sol[-1][1] = max(b, d)
+    #         else:
+    #             sol.append(intervals[i])
+    #     return sol
