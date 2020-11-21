4fab9d97541aa9ef088b5e6841e7cef725dbb7f9
Mason <mason.cao@uwaterloo.ca>
2020-11-21 15:49:48 -0500

    change to O(1) dp sol

--- algorithms/numbers_at_most_n_given_digit_set.py
+++ algorithms/numbers_at_most_n_given_digit_set.py
@@ -9,16 +9,17 @@ Memory Usage: 14.4 MB, less than 9.18% of Python3 online submissions for Numbers
 class Solution:
     def atMostNGivenDigitSet(self, digits: List[str], n: int) -> int:
         slimit = str(n)
-        dp = [0]*len(slimit) + [1]
+        prev, cur = 1, 0
         for i in reversed(range(len(slimit))):
             v = int(slimit[i])
             for num in digits:
                 num = int(num)
                 if num < v:
-                    dp[i] += len(digits)**(len(slimit)-i-1)
+                    cur += len(digits)**(len(slimit)-i-1)
                 elif num == v:
-                    dp[i] += dp[i+1]
+                    cur += prev
+            cur, prev = 0, cur
         base = 0
         for i in range(1, len(slimit)):
             base += len(digits)**i
-        return base+dp[0]
+        return base+prev
