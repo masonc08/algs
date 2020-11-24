eb6441e6a438f8ef81728c98fd1e9a2f2a584c5d
Mason <mason.cao@uwaterloo.ca>
2020-11-23 23:07:38 -0500

    add linked list cycle

--- algorithms/linked_list_cycle.py
+++ algorithms/linked_list_cycle.py
@@ -1,3 +1,10 @@
+"""
+Leetcode 141
+Runtime: 48 ms, faster than 66.51% of Python3 online submissions for Linked List Cycle.
+Memory Usage: 17.1 MB, less than 67.60% of Python3 online submissions for Linked List Cycle.
+"""
+
+
 # Definition for singly-linked list.
 # class ListNode:
 #     def __init__(self, x):
