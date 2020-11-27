48698f83cf05425c36d35e3957b06514ce7cdddc
Mason <mason.cao@uwaterloo.ca>
2020-11-27 13:38:49 -0500

    add 01 knapsack solution

--- algorithms/partition_equal_subset_sum.py
+++ algorithms/partition_equal_subset_sum.py
@@ -1,8 +1,6 @@
 """
 Leetcode 416
 November Leetcoding challenges
-Runtime: 1348 ms, faster than 50.53% of Python3 online submissions for Partition Equal Subset Sum.
-Memory Usage: 89.3 MB, less than 6.12% of Python3 online submissions for Partition Equal Subset Sum.
 """
 
 
@@ -10,20 +8,43 @@ from typing import List
 
 
 class Solution:
+    """
+    DP solution, 01 knapsack style
+    Runtime: 1152 ms, faster than 56.03% of Python3 online submissions for Partition Equal Subset Sum.
+    Memory Usage: 14.4 MB, less than 66.43% of Python3 online submissions for Partition Equal Subset Sum.
+    """
     def canPartition(self, nums: List[int]) -> bool:
         ttl = sum(nums)
         if ttl%2 != 0:
             return False
         target = ttl//2
-        cache = [[None]*ttl for _ in range(len(nums))]
-        def dfs(i, target):
-            if target == 0:
-                return True
-            if i == len(nums):
-                return False
-            if cache[i][target] is not None:
-                return cache[i][target]
-            cache[i][target] = dfs(i+1, target-nums[i]) or dfs(i+1, target)
-            return cache[i][target]
+        dp = [False]*(ttl+1)
+        dp[0] = True
+        for num in nums:
+            for j in range(target, num-1, -1):
+                dp[j] |= dp[j-num]
+        return dp[target]
 
-        return dfs(0, target)
+
+    """
+    DFS Solution with memoization
+    Runtime: 1348 ms, faster than 50.53% of Python3 online submissions for Partition Equal Subset Sum.
+    Memory Usage: 89.3 MB, less than 6.12% of Python3 online submissions for Partition Equal Subset Sum.
+    """
+    # def canPartition(self, nums: List[int]) -> bool:
+    #     ttl = sum(nums)
+    #     if ttl%2 != 0:
+    #         return False
+    #     target = ttl//2
+    #     cache = [[None]*ttl for _ in range(len(nums))]
+    #     def dfs(i, target):
+    #         if target == 0:
+    #             return True
+    #         if i == len(nums):
+    #             return False
+    #         if cache[i][target] is not None:
+    #             return cache[i][target]
+    #         cache[i][target] = dfs(i+1, target-nums[i]) or dfs(i+1, target)
+    #         return cache[i][target]
+
+    #     return dfs(0, target)
