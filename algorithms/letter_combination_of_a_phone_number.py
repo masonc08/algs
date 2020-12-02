352bd4e6313153a78b40156841d3a76706474ea8
Mason <mason.cao@uwaterloo.ca>
2020-12-02 11:08:47 -0500

    optimize algorithm

--- algorithms/letter_combination_of_a_phone_number.py
+++ algorithms/letter_combination_of_a_phone_number.py
@@ -6,21 +6,19 @@ Memory Usage: 14.1 MB, less than 100.00% of Python3 online submissions for Lette
 
 
 class Solution:
+    MP = ['0', '1', 'abc', 'def', 'ghi', 'jkl', 'mno', 'pqrs', 'tuv', 'wxyz']
     def letterCombinations(self, digits: str) -> List[str]:
         if len(digits) == 0:
             return []
-        mp = ['0', '1', 'abc', 'def', 'ghi', 'jkl', 'mno', 'pqrs', 'tuv', 'wxyz']
-        sol = []
-        self._dfs(digits, 0, mp, [], sol)
+        sol, cur = [], [None]*len(digits)
+        def dfs(depth):
+            if depth == len(digits):
+                sol.append(''.join(cur))
+                return
+            num = int(digits[depth])
+            for c in self.MP[num]:
+                cur[depth] = c
+                dfs(depth+1)
+
+        dfs(0)
         return sol
-
-
-    def _dfs(self, digits, depth, mp, li, sol):
-        if depth == len(digits):
-            sol.append(''.join(li))
-            return
-        num = int(digits[depth])
-        for c in mp[num]:
-            li.append(c)
-            self._dfs(digits, depth+1, mp, li, sol)
-            li.pop()
