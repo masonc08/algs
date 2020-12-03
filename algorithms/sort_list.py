009674b451f6d048885e78e21b8b661335f953e8
Mason <mason.cao@uwaterloo.ca>
2020-12-03 15:37:18 -0500

    revised solution for bottom up merge sort

--- algorithms/sort_list.py
+++ algorithms/sort_list.py
@@ -3,7 +3,7 @@ Leetcode 148
 Runtime: 716 ms, faster than 5.04% of Python3 online submissions for Sort List.
 Memory Usage: 29.9 MB, less than 6.25% of Python3 online submissions for Sort List.
 """
-# TODO: Bad solution, consider revising
+
 
 # Definition for singly-linked list.
 class ListNode:
@@ -11,60 +11,50 @@ class ListNode:
         self.val = val
         self.next = next
 
+
 class Solution:
     def sortList(self, head: ListNode) -> ListNode:
-        self.next = head
+        du = ListNode(next=head)
         length = self._get_length(head)
         if length < 2:
             return head
         size = 1
         while size <= length:
             size *= 2
-            main = self
+            main = du
             while main.next:
-                run = main.next
-                count = 0
-                mid = None
-                while run and count < size:
-                    if count == size//2:
-                        mid = run
-                    count += 1
-                    run = run.next
-                main.next, main = self._merge(main.next, mid, run)
-        return self.next
+                mid, end = self._get_mid_and_end(main.next, size)
+                main.next, main = self._merge(main.next, mid, end)
+        return du.next
+
+
+    def _get_mid_and_end(self, start, nnodes):
+        run, count, mid = start, 0, None
+        while run and count < nnodes:
+            if count == nnodes//2:
+                mid = run
+            count += 1
+            run = run.next
+        return mid, run
 
 
     def _merge(self, start, mid, end):
         head = runner = ListNode()
-        lrun = start
-        rrun = mid
+        lrun, rrun = start, mid
         while lrun is not mid or rrun is not end:
-            if lrun is mid:
+            if lrun is mid or (rrun is not end and lrun.val >= rrun.val):
                 if lrun is None:
                     return head.next, runner
-                runner.next = rrun
-                runner = runner.next
-                rrun = rrun.next
-            elif rrun is end:
-                runner.next = lrun
-                runner = runner.next
-                lrun = lrun.next
-            elif lrun.val < rrun.val:
-                runner.next = lrun
-                runner = runner.next
-                lrun = lrun.next
-            elif lrun.val >= rrun.val:
-                runner.next = rrun
-                runner = runner.next
-                rrun = rrun.next
-            runner.next = end
+                runner.next, runner, rrun = rrun, rrun, rrun.next
+            else:
+                runner.next, runner, lrun = lrun, lrun, lrun.next
+        runner.next = end
         return head.next, runner
 
 
     def _get_length(self, head):
-        i = head
         sol = 0
-        while i:
+        while head:
             sol += 1
-            i = i.next
+            head = head.next
         return sol
