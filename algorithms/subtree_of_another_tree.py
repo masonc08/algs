8d99a1d5f024fb988635cdf3bb4b0a126ff54c26
Mason <mason.cao@uwaterloo.ca>
2020-12-05 12:45:01 -0500

    comment code block

--- algorithms/subtree_of_another_tree.py
+++ algorithms/subtree_of_another_tree.py
@@ -38,19 +38,19 @@ class Solution:
     Runtime: 284 ms, faster than 12.37% of Python3 online submissions for Subtree of Another Tree.
     Memory Usage: 15.1 MB, less than 35.96% of Python3 online submissions for Subtree of Another Tree.
     """
-    def isSubtree(self, s: TreeNode, t: TreeNode) -> bool:
-        if not s:
-            return False
-        if self._check(s, t):
-            return True
-        return self.isSubtree(s.left, t) or self.isSubtree(s.right, t)
+    # def isSubtree(self, s: TreeNode, t: TreeNode) -> bool:
+    #     if not s:
+    #         return False
+    #     if self._check(s, t):
+    #         return True
+    #     return self.isSubtree(s.left, t) or self.isSubtree(s.right, t)
 
 
-    def _check(self, s, t):
-        if not s and not t:
-            return True
-        if bool(s) ^ bool(t):
-            return False
-        if s.val != t.val:
-            return False
-        return self._check(s.left, t.left) and self._check(s.right, t.right)
+    # def _check(self, s, t):
+    #     if not s and not t:
+    #         return True
+    #     if bool(s) ^ bool(t):
+    #         return False
+    #     if s.val != t.val:
+    #         return False
+    #     return self._check(s.left, t.left) and self._check(s.right, t.right)
