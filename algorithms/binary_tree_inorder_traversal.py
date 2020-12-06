b362a7a8fab75422cda1b9abf7e95a0ccf86af23
Mason <mason.cao@uwaterloo.ca>
2020-12-05 20:51:09 -0500

    add leetcode 94

---
+++ algorithms/binary_tree_inorder_traversal.py
@@ -0,0 +1,49 @@
+"""
+Leetcode 94
+"""
+
+
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    """
+    Iterative
+    Runtime: 32 ms, faster than 50.62% of Python3 online submissions for Binary Tree Inorder Traversal.
+    Memory Usage: 14.2 MB, less than 36.17% of Python3 online submissions for Binary Tree Inorder Traversal.
+    """
+    def inorderTraversal(self, root: TreeNode) -> List[int]:
+        if not root:
+            return []
+        stk = [root]
+        sol = []
+        while stk:
+            parent = stk[-1]
+            while parent and parent.left:
+                stk.append(parent.left)
+                parent.left = None
+                parent = stk[-1]
+            leaf = stk.pop()
+            sol.append(leaf.val)
+            leaf.right and stk.append(leaf.right)
+        return sol
+
+
+    """
+    Recursive
+    Runtime: 40 ms, faster than 50.62% of Python3 online submissions for Binary Tree Inorder Traversal.
+    Memory Usage: 14.4 MB, less than 12.66% of Python3 online submissions for Binary Tree Inorder Traversal.
+    """
+    # def inorderTraversal(self, root: TreeNode) -> List[int]:
+    #     sol = []
+    #     def inorder(node):
+    #         if not node:
+    #             return
+    #         inorder(node.left)
+    #         sol.append(node.val)
+    #         inorder(node.right)
+    #     inorder(root)
+    #     return sol
