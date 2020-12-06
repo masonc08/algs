a068e549b48ec5b2c578ab1533e3681e3b01ffcd
Mason <mason.cao@uwaterloo.ca>
2020-12-05 19:19:42 -0500

    add leetcode 617

---
+++ algorithms/merge_two_binary_trees.py
@@ -0,0 +1,21 @@
+"""
+Leetcode 617
+Runtime: 84 ms, faster than 83.26% of Python3 online submissions for Merge Two Binary Trees.
+Memory Usage: 15.6 MB, less than 10.22% of Python3 online submissions for Merge Two Binary Trees.
+"""
+
+
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def mergeTrees(self, t1: TreeNode, t2: TreeNode) -> TreeNode:
+        if not t1 or not t2:
+            return t1 or t2
+        t1.val += t2.val
+        t1.left = self.mergeTrees(t1.left, t2.left)
+        t1.right = self.mergeTrees(t1.right, t2.right)
+        return t1
