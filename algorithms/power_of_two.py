5a1748e72d2556e4f4cb70259031b0abf1bffe32
Mason <mason.cao@uwaterloo.ca>
2020-12-06 20:09:58 -0500

    add leetcode 231

---
+++ algorithms/power_of_two.py
@@ -0,0 +1,26 @@
+"""
+Leetcode 231
+"""
+
+
+class Solution:
+    def isPowerOfTwo(self, n: int) -> bool:
+        if n <= 0:
+            return False
+        return not n&(n-1)
+
+
+    """
+    O(32) runtime, O(1) space by scanning for a single 1-bit
+    Runtime: 44 ms, faster than 7.62% of Python3 online submissions for Power of Two.
+    Memory Usage: 14.1 MB, less than 79.25% of Python3 online submissions for Power of Two.
+    """
+    # def isPowerOfTwo(self, n: int) -> bool:
+    #     if n <= 0:
+    #         return False
+    #     while n:
+    #         lsb = n&1
+    #         n >>= 1
+    #         if lsb == 1:
+    #             return n == 0
+    #     return True
