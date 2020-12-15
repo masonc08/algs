aa4179710c0aac483519f690671bb297ef3876f9
Mason <mason.cao@uwaterloo.ca>
2020-12-15 09:42:53 -0500

    add lc 977

--- algorithms/squares_of_a_sorted_array.py
+++ algorithms/squares_of_a_sorted_array.py
@@ -1,22 +1,17 @@
 """
 Leetcode 977
+December Leetcoding challenge
 Runtime: 248 ms, faster than 23.51% of Python3 online submissions for Squares of a Sorted Array.
 Memory Usage: 16.3 MB, less than 6.91% of Python3 online submissions for Squares of a Sorted Array.
 """
 
 
+from bisect import bisect_left
+
+
 class Solution:
     def sortedSquares(self, nums: List[int]) -> List[int]:
-        i, j = 0, len(nums)
-        while i < j:
-            m = (i+j)//2
-            if nums[m] < 0:
-                i = m+1
-            elif nums[m] > 0:
-                j = m
-            else:
-                i = m
-                break
+        i = bisect_left(nums, 0)
         i, j = i-1, i
         sol = [0]*len(nums)
         ptr = 0
