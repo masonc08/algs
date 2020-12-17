6ae2f547dd2445806ab8094cd4d93655605e2bed
Mason <mason.cao@uwaterloo.ca>
2020-12-16 23:35:39 -0500

    add improved pascal triangle

--- algorithms/pascals_triangle.py
+++ algorithms/pascals_triangle.py
@@ -1,19 +1,14 @@
 """
 Leetcode 118
-Runtime: 52 ms, faster than 5.80% of Python3 online submissions for Pascal's Triangle.
-Memory Usage: 14.3 MB, less than 25.56% of Python3 online submissions for Pascal's Triangle.
+Runtime: 32 ms, faster than 54.00% of Python3 online submissions for Pascal's Triangle.
+Memory Usage: 14.4 MB, less than 10.02% of Python3 online submissions for Pascal's Triangle.
 """
 
 
 class Solution:
     def generate(self, numRows: int) -> List[List[int]]:
-        if numRows == 0:
-            return []
-        sol = [None]*numRows
-        sol[0] = [1]
+        sol = [[1]*i for i in range(1, numRows+1)]
         for i in range(2, numRows+1):
-            cur = [1]*i
             for j in range(1, i-1):
-                cur[j] = sol[i-2][j-1]+sol[i-2][j]
-            sol[i-1] = cur
+                sol[i-1][j] = sol[i-2][j-1]+sol[i-2][j]
         return sol
