150575f2ba66346b0622ed11ebb99bf2958d14b1
Mason <mason.cao@uwaterloo.ca>
2020-12-18 11:53:23 -0500

    add lc 334

---
+++ algorithms/increasing_triplet_subsequence.py
@@ -0,0 +1,18 @@
+"""
+Leetcode 334
+Runtime: 52 ms, faster than 76.05% of Python3 online submissions for Increasing Triplet Subsequence.
+Memory Usage: 14.9 MB, less than 7.12% of Python3 online submissions for Increasing Triplet Subsequence.
+"""
+
+
+class Solution:
+    def increasingTriplet(self, nums: List[int]) -> bool:
+        a, b = float('inf'), float('inf')
+        for n in nums:
+            if n <= a:
+                a = n
+            elif n <= b:
+                b = n
+            else:
+                return True
+        return False
