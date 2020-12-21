3f78d4e051db9842f90e4725325d9dff14f3c247
Mason <mason.cao@uwaterloo.ca>
2020-12-21 12:43:31 -0500

    code cleanup for day 20 aoc

--- advent_of_code/day20/JigsawPiece.py
+++ advent_of_code/day20/JigsawPiece.py
@@ -6,9 +6,11 @@ class JigsawPiece:
             raise Exception("Invalid input")
         self.pattern = pattern
         top, bottom = pattern[0], pattern[-1][::-1]
+        self.m, self.n = len(pattern), len(pattern[0])
         left = ''.join([ pattern[i][0] for i in range(len(pattern)-1, -1, -1) ])
         right = ''.join([ pattern[i][-1] for i in range(len(pattern)) ])
         self.sides = [ top, right, bottom, left ]
+        self.form = 0
 
     @property
     def top(self):
@@ -26,23 +28,31 @@ class JigsawPiece:
     def left(self):
         return self.sides[3]
 
-    def rotate(self):
+    def _rotate(self):
         def subarray_reverse(i=0, j=len(self.sides)-1):
             while i < j:
                 self.sides[i], self.sides[j] = self.sides[j], self.sides[i]
                 i, j = i+1, j-1
-        for _ in range(4):
-            yield
-            subarray_reverse()
-            subarray_reverse(1)
-            self.pattern = [ ''.join(arr) for arr in zip(*self.pattern[::-1]) ]
+        subarray_reverse()
+        subarray_reverse(1)
+        self.pattern = [ ''.join(arr) for arr in zip(*self.pattern[::-1]) ]
+
+    def _flip(self):
         self.pattern = [ line[::-1] for line in self.pattern ]
         top, bottom = self.pattern[0], self.pattern[-1][::-1]
         left = ''.join([ self.pattern[i][0] for i in range(len(self.pattern)-1, -1, -1) ])
         right = ''.join([ self.pattern[i][-1] for i in range(len(self.pattern)) ])
         self.sides = [ top, right, bottom, left ]
-        for _ in range(4):
-            yield
-            subarray_reverse()
-            subarray_reverse(1)
-            self.pattern = [ ''.join(arr) for arr in zip(*self.pattern[::-1]) ]
+
+    def transform(self):
+        if 0 <= self.form < 4:
+            self._rotate()
+        elif self.form == 4:
+            self._flip()
+        elif 4 < self.form < 8:
+            self._rotate()
+        else:
+            raise Exception("Unknown form", self.form)
+        self.form += 1
+        self.form %= 8
+        return tuple(self.sides)
--- advent_of_code/day20/jurassic_jigsaw.py
+++ advent_of_code/day20/jurassic_jigsaw.py
@@ -1,5 +1,5 @@
 from JigsawPiece import JigsawPiece
-from collections import defaultdict, Counter
+from collections import defaultdict
 
 
 class JurassicJigsaw:
@@ -8,23 +8,21 @@ class JurassicJigsaw:
         self.sides = defaultdict(list)
 
     def read_jigsaw(self):
-        with open('./advent_of_code/day20/jigsaw_puzzle.txt') as reader:
-            segments = reader.read().split('\n\n')
-        for segment in segments:
-            segment = segment.split('\n')
-            n = int(segment[0].split()[1][:-1])
-            puzzle_piece = segment[1:]
-            piece = JigsawPiece(n, puzzle_piece)
-            sides = [ piece.top for _ in piece.rotate() ]
-            for side in sides:
-                self.sides[side].append(n)
-            self.pieces[n] = piece
+        if not self.pieces or not self.sides:
+            with open('./advent_of_code/day20/jigsaw_puzzle.txt') as reader:
+                segments = reader.read().split('\n\n')
+            for segment in segments:
+                segment = segment.split('\n')
+                n = int(segment[0].split()[1][:-1])
+                puzzle_piece = segment[1:]
+                piece = JigsawPiece(n, puzzle_piece)
+                sides = [ piece.transform()[0] for _ in range(8) ]
+                for side in sides:
+                    self.sides[side].append(n)
+                self.pieces[n] = piece
 
-    def part1(self):
-        self.read_jigsaw()
-        m = n = int(len(self.pieces)**(1/2))
-        sol = [[None]*m for _ in range(n)]
-        ct = Counter()
+    def get_edge_piece(self):
+        ct = defaultdict(int)
         for key in self.sides:
             if len(self.sides[key]) == 1:
                 ct[self.sides[key][0]] += 1
@@ -32,166 +30,80 @@ class JurassicJigsaw:
         for key in ct:
             if ct[key] == 4:
                 edge = key
-        edge = self.pieces[edge]
-        for _ in edge.rotate():
-            if len(self.sides[edge.top]) == 1 and len(self.sides[edge.left]) == 1:
                 break
-        sol[0][0] = edge
-        edge.used = True
+        return self.pieces[edge]
+
+    def is_similar(self, a, b):
+        return a==b or a[::-1]==b
+
+    def can_use(self, i, j):
+        if i != 0 and j != 0:
+            return lambda cur, top, other=None: self.is_similar(cur.top, top.bottom) and self.is_similar(cur.left, other.right)
+        elif i == 0:
+            return lambda cur, left, other=None: self.is_similar(cur.left, left.right) and len(self.sides[cur.top]) == 1
+        elif j == 0:
+            return lambda cur, top, other=None: self.is_similar(cur.top, top.bottom) and len(self.sides[cur.left]) == 1
+        else:
+            raise Exception("Logic error")
+
+    def part1(self):
+        self.read_jigsaw()
+        m = n = int(len(self.pieces)**(1/2))
+        sol = [[None]*m for _ in range(n)]
+        edge = self.get_edge_piece()
+        while len(self.sides[edge.top]) != 1 or len(self.sides[edge.left]) != 1:
+            edge.transform()
+        sol[0][0], edge.used = edge, True
         for i in range(len(sol)):
             for j in range(len(sol[0])):
-                if i == 0 and j == 0:
-                    continue
-                if i == 0:
-                    left = sol[i][j-1]
-                    for cand_id in self.sides[left.right]:
+                if i != 0 or j != 0:
+                    neib = sol[i][j-1] if i == 0 else sol[i-1][j]
+                    other = sol[i][j-1] if i != 0 and j != 0 else None
+                    for cand_id in self.sides[neib.right if i == 0 else neib.bottom]:
                         cand = self.pieces[cand_id]
                         if not cand.used:
-                            for _ in cand.rotate():
-                                if (cand.left == left.right or cand.left[::-1] == left.right) and len(self.sides[cand.top]) == 1:
-                                    break
-                            sol[i][j] = cand
-                            cand.used = True
+                            while not self.can_use(i, j)(cand, neib, other):
+                                cand.transform()
+                            sol[i][j], cand.used = cand, True
                             break
-                elif j == 0:
-                    top = sol[i-1][j]
-                    for cand_id in self.sides[top.bottom]:
-                        cand = self.pieces[cand_id]
-                        if not cand.used:
-                            for _ in cand.rotate():
-                                if (cand.top == top.bottom or cand.top[::-1] == top.bottom) and len(self.sides[cand.left]) == 1:
-                                    break
-                            sol[i][j] = cand
-                            cand.used = True
-                            break
-                else:
-                    left = sol[i][j-1]
-                    top = sol[i-1][j]
-                    for cand_id in self.sides[left.right]:
-                        cand = self.pieces[cand_id]
-                        if not cand.used:
-                            for _ in cand.rotate():
-                                if (cand.left == left.right or cand.left[::-1] == left.right) and (cand.top == top.bottom or cand.top[::-1] == top.bottom):
-                                    break
-                            sol[i][j] = cand
-                            cand.used = True
-                            break
-        self.part1sol = sol
+        self.finished_puzzle = sol
         return sol[0][0].tile_num*sol[-1][0].tile_num*sol[-1][-1].tile_num*sol[0][-1].tile_num
 
     LOCS = (
-        (0, 0),
-        (1, 1),
-        (1, 4),
-        (0, 5),
-        (0, 6),
-        (1, 7),
-        (1, 10),
-        (0, 11),
-        (0, 12),
-        (1, 13),
-        (1, 16),
-        (0, 17),
-        (0, 18),
-        (-1, 18),
-        (0, 19)
+        (0, 0), (1, 1), (1, 4), (0, 5), (0, 6), (1, 7), (1, 10), (0, 11),
+        (0, 12), (1, 13), (1, 16), (0, 17), (0, 18), (-1, 18), (0, 19)
     )
 
-    def is_monster(self, i, j, board, visited):
+    def is_monster(self, i, j, board):
+        visited = set()
         for i1, j1 in self.LOCS:
             if i+i1 < 0 or i+i1 >= len(board) or j+j1 < 0 or j+j1 >= len(board) or board[i+i1][j+j1] != '#':
                 return set()
             visited.add((i+i1, j+j1))
         return visited
-        
+
+    def get_finished_puzzle(self):
+        self.part1()
+        m, n = len(self.finished_puzzle), len(self.finished_puzzle[0])
+        picture = []
+        for i in range(m):
+            for j in range(1, self.finished_puzzle[i][0].m-1):
+                line = []
+                for k in range(n):
+                    for m in range(1, self.finished_puzzle[i][k].n-1):
+                        line += self.finished_puzzle[i][k].pattern[j][m],
+                picture += ''.join(line),
+        return JigsawPiece(None, picture)
 
     def part2(self):
-        self.part1()
-        t = self.part1sol
-        picture = []
-        for i in range(len(t)):
-            for j in range(1, len(t[i][0].pattern)-1):
-                line = []
-                for k in range(len(t[0])):
-                    for m in range(1, len(t[i][k].pattern[j])-1):
-                        c = t[i][k].pattern[j][m]
-                        line.append(c)
-                picture.append(''.join(line))
-        puzzle = JigsawPiece(None, picture)
+        puzzle = self.get_finished_puzzle()
         monsters = set()
-        for _ in puzzle.rotate():
-            board = puzzle.pattern
-            for i in range(len(board)):
-                for j in range(len(board[0])):
-                    monsters = monsters | self.is_monster(i, j, board, set())
-            if len(monsters) != 0:
-                break
-        ct = sum(line.count('#') for line in puzzle.pattern)
-        return ct-len(monsters)
+        i = 9
+        while (i := i-1) and not monsters:
+            puzzle.transform()
+            for i in range(puzzle.m):
+                for j in range(puzzle.n):
+                    monsters.update(self.is_monster(i, j, puzzle.pattern))
+        return sum(line.count('#') for line in puzzle.pattern)-len(monsters)
 
 print(JurassicJigsaw().part2())
-
-
-    # def call_next(self, i, j, sol):
-    #     if j == len(sol[0])-1:
-    #         if self.can_make(i+1, 0, sol):
-    #             return True
-    #     else:
-    #         if self.can_make(i, j+1, sol):
-    #             return True
-    #     return False
-
-    # def can_make(self, i, j, sol):
-    #     if i == len(sol) or j == len(sol[0]):
-    #         return True
-    #     if (i == 0) ^ (j == 0):
-    #         neib_id = sol[i][j-1] if i == 0 else sol[i-1][j]
-    #         neib_piece = self.pieces[neib_id]
-    #         pattern = neib_piece.right if i == 0 else neib_piece.bottom
-    #         for cand_id in self.sides[pattern]:
-    #             cand = self.pieces[cand_id]
-    #             if not cand.used:
-    #                 sol[i][j] = cand_id
-    #                 cand.used = True
-    #                 match = (cand.left == neib_piece.right) if i == 0 \
-    #                     else (cand.top == neib_piece.bottom)
-    #                 for _ in cand.rotate():
-    #                     if match and self.call_next(i, j, sol):
-    #                         return True
-    #                 sol[i][j] = None
-    #                 cand.used = False
-    #     elif i != 0 and j != 0:
-    #         top_id = sol[i-1][j]
-    #         top_piece = self.pieces[top_id]
-    #         left_id = sol[i][j-1]
-    #         left_piece = self.pieces[left_id]
-    #         pattern = left_piece.right
-    #         for cand_id in self.sides[pattern]:
-    #             cand = self.pieces[cand_id]
-    #             if not cand.used:
-    #                 sol[i][j] = cand_id
-    #                 cand.used = True
-    #                 match = cand.left == left_piece.right and cand.top == top_piece.bottom
-    #                 for _ in cand.rotate():
-    #                     if match and self.call_next(i, j, sol):
-    #                         return True
-    #                 sol[i][j] = None
-    #                 cand.used = False
-    #     else:
-    #         raise Exception("Logic error")
-    #     return False
-
-    # def part1(self):
-    #     self.read_jigsaw()
-    #     m = n = int(len(self.pieces)**(1/2))
-    #     sol = [[None]*m for _ in range(n)]
-    #     for piece_num in self.pieces:
-    #         piece = self.pieces[piece_num]
-    #         sol[0][0] = piece_num
-    #         piece.used = True
-    #         for _ in range(4):
-    #             if self.can_make(0, 1, sol):
-    #                 return sol[0][0]*sol[0][-1]*sol[-1][0]*sol[-1][-1]
-    #             piece.rotate()
-    #         piece.used = False
-    #     raise Exception("No solution")
