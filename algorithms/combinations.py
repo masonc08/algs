9750bbd82cdf8ce04d297c16705c09e78ad7899e
Mason <mason.cao@uwaterloo.ca>
2020-12-22 21:13:07 -0500

    improve solution for lc 77

--- algorithms/combinations.py
+++ algorithms/combinations.py
@@ -7,13 +7,13 @@ Memory Usage: 15.7 MB, less than 50.59% of Python3 online submissions for Combin
 class Solution:
     def combine(self, n: int, k: int) -> List[List[int]]:
         sol, cur = [], []
-        def helper(d, i=1):
-            if d == k+1:
+        def helper(k, s=1):
+            if k == 0:
                 sol.append(cur.copy())
                 return
-            for i in range(i, n-(k-d)+1):
+            for i in range(s, n-k+2):
                 cur.append(i)
-                helper(d+1, i+1)
+                helper(k-1, i+1)
                 cur.pop()
-        helper(1)
+        helper(k)
         return sol
