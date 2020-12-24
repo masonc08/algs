7733efbb116a0f14e2f77873f42a2b8e692b6091
Mason <mason.cao@uwaterloo.ca>
2020-12-24 12:15:39 -0500

    improve solution for lc 24

--- algorithms/swap_pairs.py
+++ algorithms/swap_pairs.py
@@ -1,4 +1,9 @@
-# Leetcode 24
+"""
+Leetcode 24
+December Leetcoding challenge
+Runtime: 28 ms, faster than 82.02% of Python3 online submissions for Swap Nodes in Pairs.
+Memory Usage: 14.4 MB, less than 10.51% of Python3 online submissions for Swap Nodes in Pairs.
+"""
 
 # Definition for singly-linked list.
 # class ListNode:
@@ -7,17 +12,28 @@
 #         self.next = next
 class Solution:
     def swapPairs(self, head: ListNode) -> ListNode:
-        if head is None or head.next is None:
-            return head
-        runner = head
-        head = runner.next
-        temp = runner.next.next
-        runner.next.next = runner
-        runner.next = temp
-        while runner and runner.next and runner.next.next:
-            temp = runner.next.next
-            runner.next.next = runner.next.next.next
-            temp.next = runner.next
-            runner.next = temp
-            runner = runner.next.next
-        return head
+        self.next = head
+        run = self
+        while run and run.next and run.next.next:
+            a, b, c, d = run, run.next, run.next.next, run.next.next.next
+            a.next, b.next, c.next = c, d, b
+            run = b
+        return self.next
+
+
+# class Solution:
+#     def swapPairs(self, head: ListNode) -> ListNode:
+#         if head is None or head.next is None:
+#             return head
+#         runner = head
+#         head = runner.next
+#         temp = runner.next.next
+#         runner.next.next = runner
+#         runner.next = temp
+#         while runner and runner.next and runner.next.next:
+#             temp = runner.next.next
+#             runner.next.next = runner.next.next.next
+#             temp.next = runner.next
+#             runner.next = temp
+#             runner = runner.next.next
+#         return head
