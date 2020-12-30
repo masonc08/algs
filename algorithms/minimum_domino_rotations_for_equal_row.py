6ef514e374fc6f32a039ca6598f6838da79d29bc
Mason <mason.cao@uwaterloo.ca>
2020-12-30 17:17:15 -0500

    add lc 695

--- algorithms/minimum_domino_rotations_for_equal_row.py
+++ algorithms/minimum_domino_rotations_for_equal_row.py
@@ -1,6 +1,7 @@
 """
 Leetcode 1007
-
+Runtime: 1364 ms, faster than 10.11% of Python3 online submissions for Minimum Domino Rotations For Equal Row.
+Memory Usage: 15.3 MB, less than 32.01% of Python3 online submissions for Minimum Domino Rotations For Equal Row.
 """
 
 
