c79cb293662d5b128e7d6d8b9be11cd712f71547
Mason <mason.cao@uwaterloo.ca>
2020-12-31 15:32:42 -0500

    add lc 41, daily coding problem #3

---
+++ algorithms/first_missing_positive.py
@@ -0,0 +1,30 @@
+"""
+Daily Coding Problem #3, Leetcode 41
+Runtime: 32 ms, faster than 84.33% of Python3 online submissions for First Missing Positive.
+Memory Usage: 14.2 MB, less than 63.14% of Python3 online submissions for First Missing Positive.
+"""
+
+
+class Solution:
+    def firstMissingPositive(self, nums: List[int]) -> int:
+        base = float('inf')
+        L = len(nums)
+        positives = L
+        for n in nums:
+            if n > 0:
+                base = min(base, n)
+            else:
+                positives -= 1
+        if base > 1:
+            return 1
+        # lowest positive has to be 1
+        for i in range(L):
+            while 1 <= nums[i] <= positives and nums[i] != i+1:
+                other = nums[i]-1
+                if nums[other] == nums[i]:
+                    break
+                nums[i], nums[other] = nums[other], nums[i]
+        for i, v in enumerate(nums):
+            if v != i+1:
+                return i+1
+        return nums[-1]+1
