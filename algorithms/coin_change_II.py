4c94f85d9fb1cbcef063e8ab35d85757f8f4355b
Mason <mason.cao@uwaterloo.ca>
2021-01-01 15:11:41 -0500

    add lc 518

--- algorithms/coin_change_II.py
+++ algorithms/coin_change_II.py
@@ -1,17 +1,37 @@
 """
 Leetcode 518
-Runtime: 240 ms, faster than 46.70% of Python3 online submissions for Coin Change 2.
-Memory Usage: 14.5 MB, less than 60.43% of Python3 online submissions for Coin Change 2.
 """
 
 
+import functools
+
+
 class Solution:
-    def change(self, amount: int, coins: List[int]) -> int:
-        n = amount+1
-        sol = [0]*n
-        sol[0] = 1
-        for coin in coins:
-            for i in range(1, n):
-                if i-coin >= 0:
-                    sol[i] += sol[i-coin]
-        return sol[-1]
+    """
+    O(m*n) runtime, O(m*n) space by LRU cache
+    Runtime: 992 ms, faster than 11.92% of Python3 online submissions for Coin Change 2.
+    Memory Usage: 320.2 MB, less than 5.02% of Python3 online submissions for Coin Change 2.
+    """
+    def change(self, amount: int, coins: list[int]) -> int:
+        @functools.lru_cache(None)
+        def helper(amount, i):
+            if amount == 0:
+                return 1
+            if amount < 0 or i == len(coins):
+                return 0
+            return helper(amount-coins[i], i) + helper(amount, i+1)
+        return helper(amount, 0)
+
+    """
+    O(m*n) runtime, O(n) space, n=amount
+    Runtime: 240 ms, faster than 46.70% of Python3 online submissions for Coin Change 2.
+    Memory Usage: 14.5 MB, less than 60.43% of Python3 online submissions for Coin Change 2.
+    """
+    # def change(self, amount: int, coins: list[int]) -> int:
+    #     n = amount+1
+    #     sol = [0]*n
+    #     sol[0] = 1
+    #     for coin in coins:
+    #         for i in range(1, n):
+    #              sol[i] += sol[i-coin] if i-coin >= 0 else 0
+    #     return sol[-1]
