e7dd480972ea1c8555b6a7d6f57395ff701b86cf
Mason <mason.cao@uwaterloo.ca>
2021-01-02 17:19:21 -0500

    add lc 1379

--- algorithms/find_a_corresponding_node_of_a_binary_tree_in_a_clone_of_that_tree.py
+++ algorithms/find_a_corresponding_node_of_a_binary_tree_in_a_clone_of_that_tree.py
@@ -1,5 +1,5 @@
 """
-Leetcode 0
+Leetcode 1379
 January Leetcoding challenge
 Runtime: 624 ms, faster than 62.57% of Python3 online submissions for Find a Corresponding Node of a Binary Tree in a Clone of That Tree.
 Memory Usage: 24.2 MB, less than 44.08% of Python3 online submissions for Find a Corresponding Node of a Binary Tree in a Clone of That Tree.
