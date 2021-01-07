c792f52eea68d8aa2079d94cc7938ddc078fdfbc
Mason <mason.cao@uwaterloo.ca>
2021-01-07 15:47:10 -0500

    add lc 513

---
+++ algorithms/find_bottom_left_tree_value.py
@@ -0,0 +1,48 @@
+"""
+Leetcode 513
+"""
+
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+"""
+BFS
+Runtime: 44 ms, faster than 66.18% of Python3 online submissions for Find Bottom Left Tree Value.
+Memory Usage: 16.4 MB, less than 51.84% of Python3 online submissions for Find Bottom Left Tree Value.
+"""
+# class Solution:
+#     def findBottomLeftValue(self, root: TreeNode) -> int:
+#         li = [root]
+#         while li:
+#             new = []
+#             for node in li:
+#                 node.left and new.append(node.left)
+#                 node.right and new.append(node.right)
+#             if not new:
+#                 return li[0].val
+#             li = new
+#         raise Exception()
+
+
+"""
+DFS
+Runtime: 68 ms, faster than 5.33% of Python3 online submissions for Find Bottom Left Tree Value.
+Memory Usage: 17.1 MB, less than 13.60% of Python3 online submissions for Find Bottom Left Tree Value.
+"""
+class Solution:
+    def findBottomLeftValue(self, root: TreeNode) -> int:
+        def dfs(node, d, maxd, sol):
+            if not node:
+                return maxd, sol
+            if not node.left and not node.right:
+                if d > maxd:
+                    return d, node.val
+                return maxd, sol
+            maxd, sol = dfs(node.left, d+1, maxd, sol)
+            maxd, sol = dfs(node.right, d+1, maxd, sol)
+            return maxd, sol
+        _, sol = dfs(root, 0, -1, 0)
+        return sol
