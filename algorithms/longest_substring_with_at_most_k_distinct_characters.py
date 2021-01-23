b99e29b45e4eba1b900786734783bfa783754a9f
Mason <mxmcao@gmail.com>
2021-01-22 22:15:51 -0500

    add lc 340

--- algorithms/longest_substring_with_at_most_k_distinct_characters.py
+++ algorithms/longest_substring_with_at_most_k_distinct_characters.py
@@ -1,5 +1,8 @@
 """
-Daily Coding Problem #13
+Leetcode 340, Daily Coding Problem #13
+O(n) runtime, O(1) space
+Runtime: 112 ms, faster than 18.51% of Python3 online submissions for Longest Substring with At Most K Distinct Characters.
+Memory Usage: 14.2 MB, less than 96.34% of Python3 online submissions for Longest Substring with At Most K Distinct Characters.
 """
 
 
@@ -7,15 +10,18 @@ import collections
 
 
 class Solution:
-    def longestKDistinct(self, s, k):
+    def lengthOfLongestSubstringKDistinct(self, s: str, k: int) -> int:
+        if k == 0:
+            return 0
         ct = collections.Counter()
         i = sol = 0
         for j, c in enumerate(s):
             ct[c] += 1
             while i < j and len(ct) > k:
-                ct[i] -= 1
-                if ct[i] == 0:
-                    del ct[i]
+                c1 = s[i]
+                ct[c1] -= 1
+                if ct[c1] == 0:
+                    del ct[c1]
                 i += 1
             sol = max(j-i+1, sol)
         return sol
