199bdca1e1a1ada86c79887df6846f6579d4efc2
Mason <mxmcao@gmail.com>
2021-02-04 14:00:59 -0500

    refactor lc 904

--- algorithms/fruit_into_baskets.py
+++ algorithms/fruit_into_baskets.py
@@ -7,32 +7,52 @@ Memory Usage: 19.6 MB, less than 63.65% of Python3 online submissions for Fruit 
 
 class Solution:
     def totalFruit(self, tree: List[int]) -> int:
-        fruit_types = [-1, -1]
-        qty = [0, 0]
-        last_idx = [-1, -1]
-        i = j = 0
-        sol = 0
-        cumu = 0
-        while j < len(tree):
-            fruit = tree[j]
-            if fruit in fruit_types:
-                idx = fruit_types.index(fruit)
-                qty[idx] += 1
-                last_idx[idx] = j
-                if idx != 1:
-                    fruit_types.reverse()
-                    qty.reverse()
-                    last_idx.reverse()
-                cumu += 1
+        a = b = -1
+        bcnt = cur = sol = 0
+        for c in tree:
+            if c == b:
+                bcnt += 1
+                cur += 1
+            elif c == a:
+                bcnt = 1
+                cur += 1
+                a, b = b, a
             else:
-                tmp = last_idx.pop(0)
-                cumu -= tmp-i
-                i = tmp+1
-                fruit_types.pop(0)
-                qty.pop(0)
-                last_idx.append(j)
-                fruit_types.append(fruit)
-                qty.append(1)
-            sol = max(sol, cumu)
-            j += 1
+                cur = bcnt+1
+                bcnt = 1
+                a, b = b, c
+            sol = max(sol, cur)
         return sol
+
+
+# class Solution:
+#     def totalFruit(self, tree: List[int]) -> int:
+#         fruit_types = [-1, -1]
+#         qty = [0, 0]
+#         last_idx = [-1, -1]
+#         i = j = 0
+#         sol = 0
+#         cumu = 0
+#         while j < len(tree):
+#             fruit = tree[j]
+#             if fruit in fruit_types:
+#                 idx = fruit_types.index(fruit)
+#                 qty[idx] += 1
+#                 last_idx[idx] = j
+#                 if idx != 1:
+#                     fruit_types.reverse()
+#                     qty.reverse()
+#                     last_idx.reverse()
+#                 cumu += 1
+#             else:
+#                 tmp = last_idx.pop(0)
+#                 cumu -= tmp-i
+#                 i = tmp+1
+#                 fruit_types.pop(0)
+#                 qty.pop(0)
+#                 last_idx.append(j)
+#                 fruit_types.append(fruit)
+#                 qty.append(1)
+#             sol = max(sol, cumu)
+#             j += 1
+#         return sol
