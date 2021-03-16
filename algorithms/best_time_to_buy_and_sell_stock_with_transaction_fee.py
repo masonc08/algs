cbeefa3e2a210bee12dfbe406af681b41854c480
Mason <mxmcao@gmail.com>
2021-03-16 14:35:39 -0400

    add lc 714

--- algorithms/best_time_to_buy_and_sell_stock_with_transaction_fee.py
+++ algorithms/best_time_to_buy_and_sell_stock_with_transaction_fee.py
@@ -12,10 +12,7 @@ import collections, bisect, heapq, itertools, functools, math
 
 class Solution:
     def maxProfit(self, prices: List[int], fee: int) -> int:
-        L = len(prices)
-        if L <= 1:
-            return 0
         have, not_have = -prices[0], 0
-        for i in range(1, L):
+        for i in range(1, len(prices)):
             have, not_have = max(have, not_have-prices[i]), max(not_have, have+prices[i]-fee)
         return not_have
