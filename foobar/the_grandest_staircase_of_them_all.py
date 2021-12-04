bc96c2d84670297ab9cb000319e06b7b823ceed8
Mason <mxmcao@gmail.com>
2021-12-04 10:53:21 -0500

    add foobar lv3 question

---
+++ foobar/the_grandest_staircase_of_them_all.py
@@ -0,0 +1,81 @@
+"""
+The Grandest Staircase Of Them All
+==================================
+
+With the LAMBCHOP doomsday device finished, Commander Lambda is preparing to debut on the galactic stage -- but in order to make a grand entrance, Lambda needs a grand staircase! As the Commander's personal assistant, you've been tasked with figuring out how to build the best staircase EVER. 
+
+Lambda has given you an overview of the types of bricks available, plus a budget. You can buy different amounts of the different types of bricks (for example, 3 little pink bricks, or 5 blue lace bricks). Commander Lambda wants to know how many different types of staircases can be built with each amount of bricks, so they can pick the one with the most options. 
+
+Each type of staircase should consist of 2 or more steps.  No two steps are allowed to be at the same height - each step must be lower than the previous one. All steps must contain at least one brick. A step's height is classified as the total amount of bricks that make up that step.
+For example, when N = 3, you have only 1 choice of how to build the staircase, with the first step having a height of 2 and the second step having a height of 1: (# indicates a brick)
+
+#
+##
+21
+
+When N = 4, you still only have 1 staircase choice:
+
+#
+#
+##
+31
+ 
+But when N = 5, there are two ways you can build a staircase from the given bricks. The two staircases can have heights (4, 1) or (3, 2), as shown below:
+
+#
+#
+#
+##
+41
+
+#
+##
+##
+32
+
+Write a function called solution(n) that takes a positive integer n and returns the number of different staircases that can be built from exactly n bricks. n will always be at least 3 (so you can have a staircase at all), but no more than 200, because Commander Lambda's not made of money!
+
+-- Python cases --
+Input:
+solution.solution(200)
+Output:
+    487067745
+
+Input:
+solution.solution(3)
+Output:
+    1
+"""
+
+
+import collections
+
+
+def solution(n):
+    dp = collections.defaultdict(int)
+    dp[0, 0] = 1
+    for i in range(1, n+1):
+        for j in range(n+1):
+            dp[i, j] = dp[i-1, j]
+            if j >= i:
+                dp[i, j] += dp[i-1, j-i]
+    return dp[n, n]-1
+
+    # cache = {}
+    # def helper(n, lim):
+    #     if n <= 1:
+    #         return 1
+    #     if n > lim*(lim+1)//2:
+    #         return 0
+    #     if lim in {0, 1}:
+    #         return 1 if n == lim else 0
+    #     if (n, lim) in cache:
+    #         return cache[n, lim]
+    #     sol = 0
+    #     for i in range(2, lim+1):
+    #         sol += helper(n-i, i-1)
+    #     cache[n, lim] = sol
+    #     return sol
+    # return sum(helper(n-i, i-1) for i in range(2, n))
+
+print(solution(200))
