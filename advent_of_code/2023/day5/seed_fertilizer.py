bdde13d4d203acf7a25f6280623c991598d4b798
Mason Cao <cao.mason@uber.com>
2023-12-05 09:04:34 -0800

    now for debugging...

--- advent_of_code/2023/day5/seed_fertilizer.py
+++ advent_of_code/2023/day5/seed_fertilizer.py
@@ -5,14 +5,14 @@ def get_seeds_interval(s):
     li = tuple(map(int, s.split()[1:]))
     intervals = []
     for i in range(0, len(li), 2):
-        intervals.append((s[i], s[i+1]))
+        intervals.append((li[i], li[i+1]))
     return intervals
 
 def get_mappings(s):
     mappings = []
     for block in s:
         mapping = []
-        for line in block.split("\n")[1:]:
+        for line in block.strip().split("\n")[1:]:
             mapping.append(tuple(map(int, line.split())))
         mappings.append(mapping)
         mapping.sort(key=lambda x: x[1])
@@ -36,7 +36,7 @@ def part1():
     mapping_lines = blocks[1:]
     seeds = get_seeds(seeds_line)
     mappings = get_mappings(mapping_lines)
-    
+
     sol = 0x7fffffff
     for seed in seeds:
         prev = seed
@@ -46,39 +46,38 @@ def part1():
         sol = min(sol, prev)
     return sol
 
-
 def part2():
-    with open("advent_of_code/2023/day5/in_sample.txt") as f:
+    with open("advent_of_code/2023/day5/in_sample_modified.txt") as f:
         lines = f.read()
     blocks = lines.split("\n\n")
     seeds_line = blocks[0].strip()
     mapping_lines = blocks[1:]
     seed_intervals = get_seeds_interval(seeds_line)
     mappings = get_mappings(mapping_lines)
-    
-    sol = 0x7fffffff
 
-    def translate_interval(start, end, mappings, i):
+    sol = [111111111111]
+    def translate_interval(seed_start, seed_end, i):
         if i == len(mappings):
-            sol = min(sol, start)
+            sol[0] = min(sol[0], seed_start)
+            return
         mapping = mappings[i]
-        for j, (dest_bound, start_bound, range) in enumerate(mapping[1:]):
-            if start < start_bound:
+        for j, (dest_start, source_start, range) in enumerate(mapping[1:]):
+            source_end = source_start + range
+            dest_end = dest_start + range
+            if seed_end < source_start: # Type A
                 last_mapping = mapping[j-1]
-                last_start_bound = last_mapping[1]
-                last_range = last_mapping[2]
-                last_source_bound_end = last_start_bound + last_range
-                translate_interval(last_source_bound_end+1, min(end), mappings, i+1)
-                return
-            if start < start_bound:
-                translate_interval(start, start_bound-1)
-                if end <= start_bound+range:
-                    translate_interval(dest_bound, dest_bound+(start_bound+range-end))
-                elif end > start_bound+range:
-                    translate_interval(dest_bound, dest_bound+range)
+                prev_source_end = last_mapping[1] + last_mapping[2]
+                translate_interval(max(prev_source_end+1, seed_start), seed_end, i+1)
+            elif seed_start < source_start and seed_end <= source_end: # Type B
+                translate_interval(seed_start, source_start-1)
+                translate_interval(dest_start, dest_start+(seed_end-source_start), i+1)
+            elif source_start <= seed_start <= seed_end <= source_end: # Type C
+                translate_interval(dest_start+(seed_start-source_start), dest_start+(seed_end-source_start), i+1)
+            elif source_start <= seed_start <= source_end < seed_end: # Type D
+                translate_interval(dest_start+(seed_start-source_start), dest_end, i+1)
 
     for start, end in seed_intervals:
-        translate_interval(start, end, mappings, 0)
+        translate_interval(start, end, 0)
+    return sol[0]
 
-
-print(part1())
+print(part2())
